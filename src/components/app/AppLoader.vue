c77c5b4ee3591b5eec440ad5eb864a39c2ca9937
Jonathan CIERP <cierp.jonathan@gmail.com>
2022-05-31 14:03:40 +0200

    feat : app loader to home page

---
+++ src/components/app/AppLoader.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex items-center justify-center">
+    <i class="pi pi-spin pi-spinner" style="font-size: 3rem"></i>
+  </div>
+</template>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <main v-if="isLoaded" class="flex flex-col items-center justify-center p-4 gap-4">
+  <main v-if="isLoadedPage" class="flex flex-col items-center justify-center p-4 gap-4">
     <BaseCard width="1200px">
       <template #content>
         <div class="flex items-center gap-4">
@@ -37,12 +37,14 @@
         <h3 class="p-4 text-center text-2xl font-bold">{{ appStore.sessions.length }} Sessions</h3>
       </template>
       <template #content>
-        <div class="flex flex-col gap-4">
+        <div v-if="isLoaded" class="flex flex-col gap-4">
           <HomePageSessionItem v-for="session in appStore.sessions" :key="session.id" :session="session" />
         </div>
+        <AppLoader v-else />
       </template>
     </BaseCard>
   </main>
+  <AppLoader v-else class="h-screen" />
 </template>
 
 <script setup lang="ts">
@@ -54,14 +56,15 @@
   const appStore = useAppStore()
   const { dateStartOf, dateEndOf, formatDate } = useLuxon()
 
+  const isLoadedPage = ref<boolean>(false)
   const isLoaded = ref<boolean>(false)
   const dateValue = ref(new Date())
   const sportCenterValue = ref<SportCenter>({})
   const fieldValue = ref<Field>({})
 
   const payload = computed<FilterSessionPayload>(() => ({
-    'ngtvSessionType.id': 1,
-    'field.id': 304, //fieldValue.value.id,
+    'ngtvSessionType.id': 2,
+    'field.id': fieldValue.value.id,
     'localActualStartedAt[after]': formatDate(dateStartOf(dateValue.value), 'yyyy-MM-dd HH:mm:ss'),
     'localActualStartedAt[before]': formatDate(dateEndOf(dateValue.value), 'yyyy-MM-dd HH:mm:ss'),
     'order[localActualStartedAt]': 'asc',
@@ -84,6 +87,6 @@
     Object.assign(sportCenterValue.value, appStore.sportCenters[0])
     await onChangeSportCenter({ value: sportCenterValue })
 
-    isLoaded.value = true
+    isLoadedPage.value = true
   })
 </script>
